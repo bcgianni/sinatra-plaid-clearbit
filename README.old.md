5724285e5307c735161da6df71820c855d6124ac
bcgianni <bruno.gianni@resultadosdigitais.com.br>
2018-05-30 11:57:00 -0300

    Initialize react-admin boilerplate

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# sinatra-plaid-clearbit
+Sample sinatra app that retrieves company info from Clearbit to enrich Plaid transactions
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => <Admin dataProvider={dataProvider} />;
+
+export default App;
