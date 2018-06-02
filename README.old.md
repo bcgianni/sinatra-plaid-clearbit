86a40983fa2af04fe276c89df6242b18d1ae287e
bcgianni <bcgianni@gmail.com>
2018-06-01 22:59:45 -0300

    WIP

--- README.old.md
+++ README.old.md
@@ -1,2 +1,6 @@
 # sinatra-plaid-clearbit
 Sample sinatra app that retrieves company info from Clearbit to enrich Plaid transactions
+
+Ok, so, the react front is based on react-admin:
+
+And 
--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { fetchUtils, Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { TransactionList } from './transactions';
 
-const dataProvider = jsonServerProvider(process.env.REACT_APP_SERVER_HOST);
+
+const httpClient = (url, options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/json' });
+    }
+    // const token = localStorage.getItem('token');
+    const token = process.env.REACT_APP_PLAID_PUBLIC_TOKEN;
+    options.headers.set('Authorization', `Bearer ${token}`);
+    return fetchUtils.fetchJson(url, options);
+}
+const dataProvider = jsonServerProvider(process.env.REACT_APP_SERVER_HOST, httpClient);
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
